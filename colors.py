78e7b2b392b59bd09b079d21dba49762a2237cf5
maciejwrobel <wrblmaciej@gmail.com>
2020-10-08 18:30:45 +0200

    Initial commit

---
+++ colors.py
@@ -0,0 +1,56 @@
+GRID_COLOR = "#a39389"
+EMPTY_CELL_COLOR = "#c2b3a9"
+SCORE_LABEL_FONT = ("Helvetica", 20, "bold")
+SCORE_FONT = ("Helvetica", 20, "bold")
+GAME_OVER_FONT = ("Helvetica", 30, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLORS = {
+   2: "#daeddf",
+   4: "#9ae3ae",
+   8: "#6ce68d",
+   16: "#42ed71",
+   32: "#17e650",
+   64: "#17c246",
+   128: "#149938",
+   256: "#107d2e",
+   512: "#0e6325",
+   1024: "#0b4a1c",
+   2048: "#031f0a",
+   4096: "#000000",
+   8192: "#000000",
+   }
+
+CELL_NUMBER_COLORS = {
+    2: "#011c08",
+    4: "#011c08",
+    8: "#011c08",
+    16: "#011c08",
+    32: "#011c08",
+    64: "#f2f2f0",
+    128: "#f2f2f0",
+    256: "#f2f2f0",
+    512: "#f2f2f0",
+    1024: "#f2f2f0",
+    2048: "#f2f2f0",
+    4096: "#f2f2f0",
+    8192: "#f2f2f0",
+    }
+
+CELL_NUMBER_FONTS = {
+    2: ("Helvetica", 30, "bold"),
+    4: ("Helvetica", 30, "bold"),
+    8: ("Helvetica", 30, "bold"),
+    16: ("Helvetica", 30, "bold"),
+    32: ("Helvetica", 30, "bold"),
+    64: ("Helvetica", 30, "bold"),
+    128: ("Helvetica", 30, "bold"),
+    256: ("Helvetica", 30, "bold"),
+    512: ("Helvetica", 30, "bold"),
+    1024: ("Helvetica", 30, "bold"),
+    2048: ("Helvetica", 30, "bold"),
+    4086: ("Helvetica", 30, "bold"),
+    8192: ("Helvetica", 30, "bold"),
+}
